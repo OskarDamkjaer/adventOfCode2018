8aa4294866f18a9a6edff24464b9f5492947946f
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-12-03 12:06:00 +0100

    day2done

--- daytwo.js
+++ daytwo.js
@@ -19,6 +19,27 @@ const numOfEach = inputList //['aaaaa', 'abc']
     {},
   );
 
-const answer = [2, 3, 4].reduce((acc, curr) => acc * numOfEach[curr], 1)
+const answer = [2, 3, 4].reduce((acc, curr) => acc * numOfEach[curr], 1);
 
-console.log(answer)
+console.log(answer);
+
+for (let i = 0; i < inputList.length - 1; i++) {
+  // newline makes the list one too long
+  for (let j = 0; j < inputList.length - 1; j++) {
+    if (i == j) continue;
+    //get two words, compare them ignoring one letter att a time.
+    const firstList = inputList /*.slice().sort()*/[i];
+    const secondList = inputList /*.slice().sort()*/[j];
+    let numWrongs = 0;
+    for (let k = 0; k < firstList.length; k++) {
+      if (firstList[k] !== secondList[k]) {
+        numWrongs++;
+      }
+    }
+    if (numWrongs == 1) {
+      console.log('========= yay');
+      console.log(firstList, secondList, i, j);
+    }
+  }
+  console.log('loops', Math.floor(100* i / inputList.length), '% done');
+}
